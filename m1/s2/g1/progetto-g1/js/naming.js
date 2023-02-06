f869f36a2a671c4059b9df4b2a61c06b9acf7a8d
DarioSalvatore <dariopay69@gmail.com>
2023-02-06 15:52:00 +0100

    progetto-s2-g1

---
+++ m1/s2/g1/progetto-g1/js/naming.js
@@ -0,0 +1,25 @@
+
+
+
+/*var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);*/
+
+
+
+if (animale == 'criceto' || animale == 'coniglio') {
+    alert('Wow! è anche il mio preferito!');
+}else if (animale == 'giraffa' || animale == 'leone') {
+    console.log(animale);
+}else if (animale == 'pantera'){
+    document.write(animale);
+}else{
+    alert('Che carino!')
+}
+
